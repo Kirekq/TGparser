381f4d618c9d7b941b8b7ad10fba40752ed6ab83
Kirekq <tishin330@gmail.com>
2026-08-24 00:44:04 +0300

    Create data

---
+++ src/main/java/com/github/kirekq/pricebot/data/Product.java
@@ -0,0 +1,65 @@
+package com.github.kirekq.pricebot.data;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "productsN")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long chatId;
+    private String article;
+    private String name;
+    private Double price;
+    private Double priceNew;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(Long chatId) {
+        this.chatId = chatId;
+    }
+
+    public String getArticle() {
+        return article;
+    }
+
+    public void setArticle(String article) {
+        this.article = article;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Double getPriceNew() {
+        return priceNew;
+    }
+
+    public void setPriceNew(Double priceNew) {
+        this.priceNew = priceNew;
+    }
+}
