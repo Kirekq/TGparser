390be5597559f8cc88581af816b001dda06f3773
Kirekq <tishin330@gmail.com>
2026-08-24 00:44:33 +0300

    SQL requests

---
+++ src/main/java/com/github/kirekq/pricebot/data/ProductRepository.java
@@ -0,0 +1,15 @@
+package com.github.kirekq.pricebot.data;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Query(value = "SELECT * FROM productsN WHERE chat_id = :chatId AND price_new > price", nativeQuery = true)
+    List<Product> findIncreasedPrice(@Param("chatId") Long chatId);
+
+    @Query(value = "SELECT * FROM productsN WHERE chat_id = :chatId AND price_new < price", nativeQuery = true)
+    List<Product> findDiscountedPrice(@Param("chatId") Long chatId);
+}
