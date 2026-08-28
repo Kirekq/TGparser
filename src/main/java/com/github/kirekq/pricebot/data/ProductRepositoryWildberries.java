bdb8d2b23a69b7666614028fdea471b07e33f840
Kirekq <tishin330@gmail.com>
2026-08-28 18:55:53 +0300

    Add deleteByChatIdAndArticle function in ProductWildberries

---
+++ src/main/java/com/github/kirekq/pricebot/data/ProductRepositoryWildberries.java
@@ -0,0 +1,16 @@
+package com.github.kirekq.pricebot.data;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface ProductRepositoryWildberries extends JpaRepository<ProductWildberries, Long> {
+    long deleteByChatIdAndArticle(Long chatId, String article);
+    @Query(value = "SELECT * FROM productsWildberries WHERE chat_id = :chatId AND price_new > price", nativeQuery = true)
+    List<ProductWildberries> findIncreasedPrice(@Param("chatId") Long chatId);
+
+    @Query(value = "SELECT * FROM productsWildberries WHERE chat_id = :chatId AND price_new < price", nativeQuery = true)
+    List<ProductWildberries> findDiscountedPrice(@Param("chatId") Long chatId);
+}
