1752da08e3dbcddce30ef4344fec4a0768add70d
Kirekq <tishin330@gmail.com>
2026-08-28 19:31:06 +0300

    Add existsByChatIdAndArticle function

--- src/main/java/com/github/kirekq/pricebot/data/ProductRepository.java
+++ src/main/java/com/github/kirekq/pricebot/data/ProductRepository.java
@@ -8,6 +8,7 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     long deleteByChatIdAndArticle(Long chatId, String article);
+    boolean existsByChatIdAndArticle(Long chatId, String article);
     @Query(value = "SELECT * FROM productsN WHERE chat_id = :chatId AND price_new > price", nativeQuery = true)
     List<Product> findIncreasedPrice(@Param("chatId") Long chatId);
 
